c7a6dc1af3d481b2d8d812239f781bee5cab81cc
ciolacuvalentin <ciolacuvalentin@yahoo.com>
2021-11-17 09:16:38 +0200

    add spiner

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getForecastFiveDays, getWeather } from "../../services/api";
 import { FormControl, Button, Form } from "react-bootstrap";
-import "./home.css"
+import Spinner from "react-bootstrap/Spinner";
+import "./home.css";
 
 // getForecastFiveDays().then(res => (console.log("Forecast",res)))
 // getWeather().then(res => (console.log("weather",res)))
@@ -13,11 +14,7 @@ const Home = () => {
   const [forecast, setForecast] = useState();
 
   useEffect(() => {
-    getWeather(city).then((res) => {
-        console.log(res)
-        if(res.cod == 200 ) setWeather(res)
-        else console.log(res.message)
-    });
+    getWeather(city).then((res) => setWeather(res));
     getForecastFiveDays(city).then((res) => setForecast(res));
   }, []);
 
@@ -26,23 +23,38 @@ const Home = () => {
     setCity(city);
   }
   function handleClickSearch() {
-    setWeather(undefined)
-    getWeather(city).then((res) => setWeather(res));
-    getForecastFiveDays(city).then((res) => setForecast(res));
-    setCity("")
+    getWeather(city).then((res) => {
+      console.log("rezultat weather", res.cod);
+      if (res.cod === 200) setWeather(res);
+      else {
+        console.log(res.message);
+        console.log("rezultat weather", res.message);
+      }
+    });
+    getForecastFiveDays(city).then((res) => {
+      console.log("rezultat forecast", res.cod);
+      if (res.cod === 200) setForecast(res);
+      else {
+        console.log(res.message);
+        console.log("rezultat forecast", res.message);
+      }
+    });
+    setWeather(null);
+    setForecast(undefined);
+    setCity("");
   }
 
-  console.log("weather",weather);
-  console.log("forecast",forecast);
+  console.log("weather", weather);
+  console.log("forecast", forecast);
   return (
     <div className="home_stile">
-      <Form inline  bg="dark" variant="dark" className="d-flex justify-content-center">
-        <FormControl
-          type="text"
-          onChange={handleInputOnChange}
-          value={city}
-        //   placeholder={city}
-        />
+      <Form
+        inline
+        bg="dark"
+        variant="dark"
+        className="d-flex justify-content-center"
+      >
+        <FormControl type="text" onChange={handleInputOnChange} value={city} />
         <Button
           variant="outline-success btn-dark"
           className="ml-2"
@@ -51,25 +63,49 @@ const Home = () => {
           Search
         </Button>
       </Form>
+      {/* { if (weather === null) <div> nu exista orasul </div> 
+elseif (weather === undefined) <Spinner animation="border" variant="primary" className="spinner_center" />
+else ( <div className="d-flex justify-content-center">
+                        <div>
+                                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} height="200"/>
+                                <div>Name : {weather.name}</div> 
+                                <div>Description : {weather.weather[0].description}</div>
+                                <div>Temperature : {weather.main.temp} °C</div>
+                                <div>Humidity : {weather.main.humidity} %</div>
+                                <div>Pressure : {weather.main.pressure} hPa</div>
+                                <div>Visibility : {weather.visibility/1000} km</div>
+                                <div>Speed wind: {weather.wind.speed} km/h</div>
+                        </div>
+                <div>
+
+                </div>
+        </div>
+
+)} */}
+
       {weather ? (
         <div className="d-flex justify-content-center">
-            <div>
-          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} height="200"/>
-           <div>Name : {weather.name}</div> 
-          <div>Description : {weather.weather[0].description}</div>
-          <div>Temperature : {weather.main.temp} °C</div>
-          <div>Humidity : {weather.main.humidity} %</div>
-          <div>Pressure : {weather.main.pressure} hPa</div>
-          <div>Visibility : {weather.visibility/1000} km</div>
-          <div>Speed wind: {weather.wind.speed} km/h</div>
-          </div>
           <div>
-         
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              height="200"
+            />
+            <div>Name : {weather.name}</div>
+            <div>Description : {weather.weather[0].description}</div>
+            <div>Temperature : {weather.main.temp} °C</div>
+            <div>Humidity : {weather.main.humidity} %</div>
+            <div>Pressure : {weather.main.pressure} hPa</div>
+            <div>Visibility : {weather.visibility / 1000} km</div>
+            <div>Speed wind: {weather.wind.speed} km/h</div>
           </div>
+          <div></div>
         </div>
-        
       ) : (
-        "...loading weather"
+        <Spinner
+          animation="border"
+          variant="primary"
+          className="spinner_center"
+        />
       )}
     </div>
   );
